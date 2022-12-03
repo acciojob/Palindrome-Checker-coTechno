cfd42d4386e223927e67132ed63344deaa8e6514
Zehra Khan <khan77040307@gmail.com>
2022-12-03 19:30:19 +0530

    done

--- solution.js
+++ solution.js
@@ -7,7 +7,8 @@ let reverse = ''
 for(let i= string.length()-1; i>=0; --i){
     reverse += string.charAt(i)
 }
-string.equals(reverse) ? console.log('String is Palondrome.') : console.log('String is Not Palondrome.')
+if (string.equals(reverse)) return true
 
+return false
 }
 module.exports = palindrome
