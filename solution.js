78cd6e4268acda6560e8677d5f6aab6a205603f8
Zehra Khan <khan77040307@gmail.com>
2022-12-03 19:59:18 +0530

    .

--- solution.js
+++ solution.js
@@ -4,7 +4,7 @@ function palindrome(str){
 let string = str.toLowerCase()
 
 let reverse = ''
-for(let i= string.length()-1; i>=0; --i){
+for(let i= string.length-1; i>=0; --i){
     reverse += string.charAt(i)
 }
 if (string === reverse) return true
