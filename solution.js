c943df8d51d65e8585d0578bbf8aa64c174ce340
Zehra Khan <khan77040307@gmail.com>
2022-12-03 19:25:25 +0530

    Done

--- solution.js
+++ solution.js
@@ -1,6 +1,13 @@
 // complete the given function
 
 function palindrome(str){
+let string = str.toLowerCase()
+
+let reverse = ''
+for(let i= string.length()-1; i>=0; --i){
+    reverse += string.charAt(i)
+}
+string.equals(reverse) ? console.log('String is Palondrome.') : console.log('String is Not Palondrome.')
 
 }
 module.exports = palindrome
